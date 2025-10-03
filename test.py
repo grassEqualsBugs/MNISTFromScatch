27eb5516078b8b2ad590dbbb30e07fd9e0a32bc4
grassEqualsBugs <eizprintz@gmail.com>
2025-10-03 09:21:08 -0700

    new big models--relu is up to 98.1%

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 from nn.neuralnet import NeuralNetwork, load_architecture
 from mnist.loader import load_mnistdata
 
-model_name = "four_layers_sigmoid"
+model_name = "four_layers_relu"
 model_path = f"models/{model_name}"
-weights_path = f"{model_path}/weights_and_biases/30e10m3.0l.npz"
+weights_path = f"{model_path}/weights_and_biases/60e30m0.1l.npz"
 architecture_path = f"{model_path}/architecture.json"
 
 layer_spec, cost_func_name, weight_init_name = load_architecture(architecture_path)
--- train.py
+++ train.py
@@ -3,10 +3,10 @@ import os
 import time
 
 from nn.neuralnet import LayerSpec, NeuralNetwork
-from nn.util import Sigmoid
+from nn.util import ReLU, Softmax
 from mnist.loader import load_mnistdata
 
-model_name = "four_layers_sigmoid"
+model_name = "four_layers_relu"
 model_path = f"models/{model_name}"
 weights_path = f"{model_path}/weights_and_biases"
 architecture_path = f"{model_path}/architecture.json"
@@ -17,12 +17,12 @@ os.makedirs(weights_path, exist_ok=True)
 # define network architecture
 layer_spec: LayerSpec = [
     (28 * 28, None),
-    (100, Sigmoid),
-    (30, Sigmoid),
-    (10, Sigmoid),
+    (100, ReLU),
+    (30, ReLU),
+    (10, Softmax),
 ]
 nn = NeuralNetwork(
-    layer_spec, cost_func_name="QuadraticCost", weight_init_name="Standard"
+    layer_spec, cost_func_name="CrossEntropyCost", weight_init_name="Kaiming"
 )
 
 # load data
@@ -31,11 +31,11 @@ nn.load_data(mnistdata["train_images"], mnistdata["train_labels"])
 
 # train the network
 before_training = time.time()
-nn.train(30, 10, np.float64(3.0))
+nn.train(60, 30, np.float64(0.1))
 print(
-    f"Training complete, total training time is {time.time() - before_training} seconds."
+    f"Training complete, total training time was {time.time() - before_training} seconds."
 )
 
 # save the trained model
 nn.save_architecture(architecture_path)
-nn.save_weights_and_biases(f"{weights_path}/30e10m3.0l.npz")
+nn.save_weights_and_biases(f"{weights_path}/60e30m0.1l.npz")
